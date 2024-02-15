309e1c9e1c8afa93df863d2abe52b24b8c311f99
Antily15503 <theminecrafter90000@gmail.com>
2024-02-14 18:07:06 -0600

    Added pages corresponding to different sections on website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
+import './App.css'
 
 const App = () => {
   return (
     <div className="app">
         <div className="navbar">
-            
+            <Navbar/>
         </div>
         <div className="main">
-
+            <Layout>
+                <div className="routes">
+                    <Routes> {/* routes allows you to have multiple routes*/}
+                        <Route exact path="/">
+                            <Homepage />
+                        </Route>
+                        <Route exact path="/exchanges">
+                            <Exchanges />
+                        </Route>
+                        <Route exact path="/cryptocurrencies">
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path="/crypto/:coinId"> {/*:coinId means the link can be dynamic (id can be 1, 2, etc.)*/}
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path="/news">
+                            <News />
+                        </Route>
+                    </Routes>
+                </div>
+            </Layout>
         </div>
         <div className="footer">
-            
+
         </div>
     </div>
   );
